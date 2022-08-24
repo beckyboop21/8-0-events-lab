b4b5b89c4af52cac51d218d8d7f74be8d831b476
Rebecca Garcia <shelteredheart21@gmail.com>
2022-08-24 03:02:40 -0400

    done, added mousedown feature

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+let currentColor = document.querySelector("#current-color");
+
+//try let palette = document.querySelectorAll("#palette" .color);
+
+let colors = document.querySelectorAll(".color");
+//console.log(palette);
+
+for (const color of colors) {
+  color.addEventListener("click", () => {
+    currentColor.style.background = color.style.background;
+  });
+}
+
+const cell = document.querySelectorAll(".cell");
+
+cell.forEach((cell) => {
+  cell.addEventListener("mousedown", (e) => {
+    console.log(" You are holding the mouse down!");
+    cell.style.background = currentColor.style.backgroundColor;
+  });
+});
+
+//console.log("height", window.innerHeight);
+//console.log("width",window.innerWidth);
